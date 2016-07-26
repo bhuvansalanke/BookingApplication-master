64c2103bb33f5e32e860805a7eee43926a3d784e
bhuvansalanke <bhuvansalanke@gmail.com>
2016-07-27 00:01:47 +0530

    assets

--- config/env/production.js
+++ config/env/production.js
@@ -12,6 +12,51 @@ module.exports = {
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
   },
+  assets: {
+    lib: {
+      css: [
+        'public/lib/bootstrap/dist/css/bootstrap.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/fullcalendar/dist/fullcalendar.min.css',
+        'public/lib/angular-motion/dist/angular-motion.min.css',
+        'public/lib/angular-multi-select-alexandernst/dist/angular-multi-select.min.css',
+        'public/lib/jquery-timepicker-jt/jquery.timepicker.min.css',
+        'public/lib/angular-material/angular-material.min.css',
+        'public/lib/font-awesome/font-awesome.min.css', 
+        'public/lib/angular-input-stars-directive/angular-input-stars.min.css',
+        'public/lib/angularMultipleSelect/build/multiple-select.min.css'
+      ],
+      js: [
+        'public/lib/angular/angular.min.js',
+        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angular-animate/angular-animate.min.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-ui-utils/ui-utils.min.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/jquery-ui/jquery-ui.min.js',
+        'public/lib/angular-route/angular-route.min.js',
+        'public/lib/angular-ui-calendar/src/calendar.min.js',
+        'public/lib/moment/moment.min.js',
+        'public/lib/angular-moment/angular-moment.min.js',
+        'public/lib/fullcalendar/dist/fullcalendar.min.js',    
+        'public/lib/angular-cookies/angular-cookies.min.js',
+        'public/lib/angular-filter/dist/angular-filter.min.js',
+        'public/lib/angular-multi-select-alexandernst/dist/angular-multi-select.min.js',
+        'public/lib/angular-strap/dist/angular-strap.min.js',
+        'public/lib/angular-strap/dist/angular-strap.tpl.min.js',
+        'public/lib/jquery-timepicker-jt/jquery.timepicker.min.js',
+        'public/lib/angular-jquery-timepicker/src/timepickerdirective.min.js',
+        'public/lib/angular-aria/angular-aria.min.js',
+        'public/lib/angular-material/angular-material.min.js',
+        'public/lib/angular-input-stars-directive/angular-input-stars.min.js',
+        'public/lib/angularMultipleSelect/build/multiple-select.min.js'
+      ]
+    },
+    css: 'public/dist/application.min.css',
+    js: 'public/dist/application.min.js'
+  },
   log: {
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
     format: 'combined',
